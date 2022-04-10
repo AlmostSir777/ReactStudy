ed10762c3b54bd60db47160aa6ee3e2eafc9b220
AlmostSir777 <774511860@qq.com>
2022-04-10 22:21:11 +0800

    router路由尝试理解

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Button } from "antd";
 import './App.css'
 import Hello from './Hello'
 import Home from './Home'
+import Demo from './Demo'
 
 function App() {
 	return (
 		<div className="App">
 			<Hello message="给我一杯茶"></Hello>
-			<Home childCount={10}></Home>
+			<Home childCount={20}></Home>
+            <Button type='primary'>66666</Button>
+            {/* <Demo></Demo> */}
 		</div>
 	)
 }
